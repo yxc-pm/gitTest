3c9d312cea0897b9cd961ba5799e0b744e8d0337
yxc-pm <yxc_pm@163.com>
2018-05-16 20:48:02 +0800

    add java

---
+++ src/com/atguigu/bean/Prrson.java
@@ -0,0 +1,11 @@
+package com.atguigu.bean;
+
+public class Prrson {
+	private String name;
+	private int age;
+	@Override
+	public String toString() {
+		return "Prrson [name=" + name + ", age=" + age + "]";
+	}
+	
+}
